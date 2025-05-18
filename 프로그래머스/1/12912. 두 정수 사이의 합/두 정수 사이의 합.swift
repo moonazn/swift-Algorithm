657706ec9955c80a7c1a29ee6969c89b4af65829
moonazn <101088300+moonazn@users.noreply.github.com>
2025-05-18 16:54:02 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
@@ -1,10 +1,16 @@
 func solution(_ a:Int, _ b:Int) -> Int64 {
     var result = 0
     
-    // for num in min(a, b)...max(a, b)
-    for num in a < b ? a...b : b...a {
-        result += num
-    }
+    // 1) for num in min(a, b)...max(a, b)
+    // 2) for num in a < b ? a...b : b...a {
+    //     result += num
+    // }
+    
+    
+    // 3)
+    let minVal = min(a, b)
+    let maxVal = max(a, b)
+    result = (minVal + maxVal) * (maxVal - minVal + 1) / 2
     
     return Int64(result)
 }
